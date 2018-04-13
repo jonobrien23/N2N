b4aa5203b4271907a5d9f6358ba53cab5927e47a
jonobrien23 <35141256+jonobrien23@users.noreply.github.com>
2018-04-13 16:12:37 +0100

    Added code uo to Ch4 and included all source answers to compare against

--- main.js
+++ main.js
@@ -1,1 +1,40 @@
 alert('Welcome to Quiz Ninja!');
+
+const quiz = [
+    ["What is Superman's real name?", "Clark Kent"],
+    ["What is Wonder Woman's real name?","Diana Prince"],
+    ["What is Batman's real name?","Bruce Wayne"]
+];
+
+function start(quiz){
+let score = 0;
+
+// main game loop
+for (const [question,answer] of quiz){
+    const response = ask(question);
+    check(reponse,answer);
+}
+// end of main loop
+
+gameOver();
+
+// function declerations
+
+function ask(question){
+    return prompt(question);
+}
+
+function check(response, answer){
+    if (response === answer){
+        alert (`Correct!`);
+        score++;
+    } else {
+        alert (`Wrong! The correct answer is ${answer}`);
+    }
+}
+
+function gameOver(){
+    alert (`Game Over! You scored ${score} point${score !== 1 ? 's' : ''}`);
+}
+}
+start(quiz);
