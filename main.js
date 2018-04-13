91d84b8b2480109978222a380f82881a8c505bb0
jonobrien23 <35141256+jonobrien23@users.noreply.github.com>
2018-04-13 16:39:57 +0100

    Updated main.js with new code - Still to fix after first question

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-alert('Welcome to Quiz Ninja!');
-
 const quiz = [
     ["What is Superman's real name?", "Clark Kent"],
     ["What is Wonder Woman's real name?","Diana Prince"],
@@ -7,34 +5,34 @@ const quiz = [
 ];
 
 function start(quiz){
-let score = 0;
-
-// main game loop
-for (const [question,answer] of quiz){
-    const response = ask(question);
-    check(reponse,answer);
-}
-// end of main loop
-
-gameOver();
-
-// function declerations
-
-function ask(question){
-    return prompt(question);
-}
-
-function check(response, answer){
-    if (response === answer){
-        alert (`Correct!`);
-        score++;
-    } else {
-        alert (`Wrong! The correct answer is ${answer}`);
+    let score = 0;
+ 
+    // main game loop
+    for (const [question,answer] of quiz){
+       const response = ask(question);
+       check(reponse,answer);
+    }
+    // end of main loop
+    
+    gameOver();
+    
+    // function declerations   
+    function ask(question){
+       return prompt(question);
+    }
+    
+    function check(response,answer){
+       if (response === answer){
+           alert (`Correct!`);
+           score++;
+       } else {
+           alert (`Wrong! The correct answer is ${answer}`);
+       };
+    }
+    
+    function gameOver(){
+       alert (`Game Over! You scored ${score} point${score !== 1 ? 's' : ''}`);
     }
 }
 
-function gameOver(){
-    alert (`Game Over! You scored ${score} point${score !== 1 ? 's' : ''}`);
-}
-}
 start(quiz);
